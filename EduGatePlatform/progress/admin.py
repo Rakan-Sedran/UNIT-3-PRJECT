bb901135d2a7b85a5b3c60e3c1564320543e14a7
Rakan Sedran <srakan595@gmail.com>
2025-12-02 01:28:15 +0300

    register progress models in admin and add teacher classsubject template

--- EduGatePlatform/progress/admin.py
+++ EduGatePlatform/progress/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Lesson, Homework, Quiz, HomeworkSubmission, QuizSubmission
+
+# Register your models here.
+
+admin.site.register(Lesson)
+admin.site.register(Homework)
+admin.site.register(Quiz)
+admin.site.register(HomeworkSubmission)
+admin.site.register(QuizSubmission)
 
 # Register your models here.
